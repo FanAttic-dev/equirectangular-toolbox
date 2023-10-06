91adbeb1e41626cebdb892e5f6be4228027a8cec
Attila Zsiros <attila.zsiros@mail.muni.cz>
2023-10-06 12:03:13 +0200

    use aspect ratio

--- nfov.py
+++ nfov.py
@@ -18,26 +18,30 @@ import cv2
 
 
 class NFOV():
-    def __init__(self, height=400, width=800):
-        self.fov = 30
+    def __init__(self, height=540, width=960):
+        self.fov = np.deg2rad(50)
         # self.limit_horiz = pi
-        self.limit_horiz = np.deg2rad(100/2)
+        self.limit_horiz = np.deg2rad(115/2)
         # self.limit_vert = pi * 0.5
-        self.limit_vert = np.deg2rad(80/2)
+        self.limit_vert = np.deg2rad(66/2)
         self.height = height
         self.width = width
         self.screen_points = self._get_screen_img()
 
     @property
     def FOV(self):
-        return [np.deg2rad(self.fov), np.deg2rad(self.fov)]
+        return np.array([self.fov, self.fov / 16 * 9])
+
+    @property
+    def limits(self):
+        return np.array([self.limit_horiz, self.limit_vert])
 
     def _get_coord_rad(self, isCenterPt, center_point=None):
-        return (center_point * 2 - 1) * np.array([self.limit_horiz, self.limit_vert]) \
+        return (center_point * 2 - 1) * self.limits \
             if isCenterPt \
             else \
-            (self.screen_points * 2 - 1) * np.array([self.limit_horiz, self.limit_vert]) * (
-                np.ones(self.screen_points.shape) * self.FOV)
+            (self.screen_points * 2 - 1) * self.limits * \
+            (self.FOV / (self.limits * 2) * np.ones(self.screen_points.shape))
 
     def _get_screen_img(self):
         xx, yy = np.meshgrid(np.linspace(0, 1, self.width),
@@ -48,11 +52,47 @@ class NFOV():
         x = convertedScreenCoord.T[0]
         y = convertedScreenCoord.T[1]
 
-        lon = x
-        lat = y
+        # lon = x + self.cp[0]
+        # lat = y + self.cp[1]
+
+        center_pan_rad = -self.cp[0]
+        center_tilt_rad = -self.cp[1]
+        lambda_rad = x
+        phi_rad = y
+        cos_c = np.sin(center_tilt_rad) * np.sin(phi_rad) + np.cos(center_tilt_rad) * \
+            np.cos(phi_rad) * np.cos(lambda_rad - center_pan_rad)
+        lon = (np.cos(phi_rad) *
+               np.sin(lambda_rad - center_pan_rad)) / cos_c
+        lat = (np.cos(center_tilt_rad) * np.sin(phi_rad) - np.sin(center_tilt_rad)
+               * np.cos(phi_rad) * np.cos(lambda_rad - center_pan_rad)) / cos_c
+
+        # rou = np.sqrt(x ** 2 + y ** 2)
+        # c = np.arctan(rou)
+        # # c = np.arcsin(rou / self.r)
+        # sin_c = np.sin(c)
+        # cos_c = np.cos(c)
+
+        # lat = np.arcsin(
+        #     cos_c * np.sin(self.cp[1]) + (y * sin_c * np.cos(self.cp[1])) / rou)
+        # lon = self.cp[0] + np.arctan2(x * sin_c, rou * np.cos(self.cp[1])
+        #                               * cos_c - y * np.sin(self.cp[1]) * sin_c)
+
+        # [-pi, pi] -> [0, 1]
+        lon = (lon / self.limit_horiz + 1.) * 0.5
+        lat = (lat / self.limit_vert + 1.) * 0.5
+
+        return np.array([lon, lat]).T
+
+    def _calcSphericaltoGnomonic_inverse(self, convertedScreenCoord):
+        x = convertedScreenCoord.T[0]
+        y = convertedScreenCoord.T[1]
+
+        # lon = x + self.cp[0]
+        # lat = y + self.cp[1]
 
         rou = np.sqrt(x ** 2 + y ** 2)
         c = np.arctan(rou)
+        # c = np.arcsin(rou / self.r)
         sin_c = np.sin(c)
         cos_c = np.cos(c)
 
@@ -86,7 +126,7 @@ class NFOV():
         C_idx = np.add(base_y0, x2)
         D_idx = np.add(base_y2, x2)
 
-        flat_img = np.reshape(self.frame, [-1, self.frame_channel])
+        flat_img = np.reshape(self.frame_orig, [-1, self.frame_channel])
 
         A = np.take(flat_img, A_idx, axis=0)
         B = np.take(flat_img, B_idx, axis=0)
@@ -110,36 +150,62 @@ class NFOV():
         # plt.show()
         return nfov
 
-    def toNFOV(self, frame, center_point):
-        self.frame = frame
-        self.frame_height = frame.shape[0]
-        self.frame_width = frame.shape[1]
-        self.frame_channel = frame.shape[2]
+    def draw_coords_(self, frame_orig, sphericalCoord):
+        xs = np.floor(sphericalCoord.T[0] * self.frame_width).astype(np.int32)
+        ys = np.floor(sphericalCoord.T[1] * self.frame_height).astype(np.int32)
+
+        skip = 150
+
+        for x, y in zip(xs[::skip], ys[::skip]):
+            cv2.circle(frame_orig, [x, y], radius=5,
+                       color=[255, 0, 255], thickness=-1)
+
+        return frame_orig
+
+    def toNFOV(self, frame_orig, center_point):
+        self.frame_orig = frame_orig
+        self.frame_height = frame_orig.shape[0]
+        self.frame_width = frame_orig.shape[1]
+        self.frame_channel = frame_orig.shape[2]
 
         self.cp = self._get_coord_rad(
             center_point=center_point, isCenterPt=True)
         convertedScreenCoord = self._get_coord_rad(isCenterPt=False)
-        spericalCoord = self._calcSphericaltoGnomonic(convertedScreenCoord)
-        return self._bilinear_interpolation(spericalCoord)
+        sphericalCoord = self._calcSphericaltoGnomonic(convertedScreenCoord)
+        frame_painted = self.draw_coords_(frame_orig.copy(), sphericalCoord)
+        return self._bilinear_interpolation(sphericalCoord), frame_painted
+
+    def get_stats(self):
+        return {
+            "limit_horiz": np.rad2deg(self.limit_horiz*2),
+            "limit_vert": np.rad2deg(self.limit_vert*2),
+            "fov": np.rad2deg(self.FOV),
+        }
 
 
 # test the class
 if __name__ == '__main__':
     import imageio as im
     frame_orig = im.imread('images/pitch.png')
+    frame_orig = cv2.cvtColor(frame_orig, cv2.COLOR_RGB2BGR)
     nfov = NFOV()
+    WINDOW_FLAGS = cv2.WINDOW_NORMAL  # cv2.WINDOW_AUTOSIZE
+    cv2.namedWindow("frame", WINDOW_FLAGS)
+    cv2.namedWindow("frame_orig", WINDOW_FLAGS)
     # camera center point (valid range [0,1])
     center_point = np.array([0.5, 0.5])
     dx = 0.01
-    dz = 1
+    dz = .01
+    dfov = .01
     while True:
         try:
-            img = nfov.toNFOV(frame_orig, center_point)
-            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            img, frame_orig_painted = nfov.toNFOV(frame_orig, center_point)
         except Exception as e:
             print(e)
 
         cv2.imshow('frame', img)
+        cv2.imshow("frame_orig", frame_orig_painted)
+        print(nfov.get_stats())
         key = cv2.waitKey(0)
         if key == ord('d'):
             center_point += np.array([dx, 0])
@@ -153,6 +219,14 @@ if __name__ == '__main__':
             nfov.fov -= dz
         elif key == ord('-'):
             nfov.fov += dz
+        elif key == ord('8'):
+            nfov.limit_vert += dfov
+        elif key == ord('2'):
+            nfov.limit_vert -= dfov
+        elif key == ord('6'):
+            nfov.limit_horiz += dfov
+        elif key == ord('4'):
+            nfov.limit_horiz -= dfov
         elif key == ord('q'):
             break
 
